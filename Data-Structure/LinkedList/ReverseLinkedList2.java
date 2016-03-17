f06b1b85f52eefce703b6162b5bb5b3dd63f5f6e
xxxxxl <lix0212@hotmail.com>
2016-03-17 17:50:26 -0400

    Create ReverseLinkedList2.java

---
+++ Data-Structure/LinkedList/ReverseLinkedList2.java
@@ -0,0 +1,27 @@
+public class Solution {
+    /**
+     * @param ListNode head is the head of the linked list 
+     * @oaram m and n
+     * @return: The head of the reversed ListNode
+     */
+    public ListNode reverseBetween(ListNode head, int m , int n) {
+        // write your code
+    
+        
+        ListNode curM = head;
+        for (int i = 0; i < m; i++){
+            while (curM != null){
+                ListNode temp = cur.next;
+                 curM = temp;
+            }
+            return head;
+        }
+        ListNode startM = curM;
+        ListNode pre = null;
+        ListNode curBetween = null;
+
+        for (int i = m; i < n; i++){
+            while(i > )
+        }
+    }
+}
