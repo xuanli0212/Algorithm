790eba382cdc3794cb0aa39aa0f0a88517c0a34e
xxxxxl <lix0212@hotmail.com>
2016-03-23 00:13:19 -0400

    Update ReverseLinkedList2.java

--- Data-Structure/LinkedList/ReverseLinkedList2.java
+++ Data-Structure/LinkedList/ReverseLinkedList2.java
@@ -1,27 +1,49 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     /**
      * @param ListNode head is the head of the linked list 
-     * @oaram m and n
+     * @param m and n
      * @return: The head of the reversed ListNode
      */
     public ListNode reverseBetween(ListNode head, int m , int n) {
         // write your code
-    
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        head = pre;
         
-        ListNode curM = head;
-        for (int i = 0; i < m; i++){
-            while (curM != null){
-                ListNode temp = cur.next;
-                 curM = temp;
-            }
-            return head;
+        for (int i = 1; i < m; i++) {
+            if ( head != null) {
+                head = head.next;
+            }else return null;
         }
-        ListNode startM = curM;
-        ListNode pre = null;
-        ListNode curBetween = null;
+        
+        ListNode preM = head;
+        ListNode Mnode = head.next;
+        ListNode Nnode = Mnode;
+        ListNode postN = Mnode.next;
 
-        for (int i = m; i < n; i++){
-            while(i > )
+        for (int i = m; i < n; i++) {
+            if( postN != null) {
+                ListNode temp = postN.next;
+                postN.next = Nnode;
+                Nnode = postN;
+                postN = temp;
+            }else return null;
         }
+        Mnode.next=postN;
+        preM.next = Nnode;
+        
+        return pre.next;
+        
     }
 }
