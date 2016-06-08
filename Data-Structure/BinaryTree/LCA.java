7125b6ad13a119618791465bdfe305eae79de349
xxxxxl <lix0212@hotmail.com>
2016-06-07 21:57:28 -0400

    Create LCA.java

---
+++ Data-Structure/BinaryTree/LCA.java
@@ -0,0 +1,41 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of the binary search tree.
+     * @param A and B: two nodes in a Binary.
+     * @return: Return the least common ancestor(LCA) of the two nodes.
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode A, TreeNode B) {
+        // write your code here
+        //TreeNode ancestor = new TreeNode();
+        if (root == null ){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, A, B);
+        TreeNode right = lowestCommonAncestor(root.right, A, B);
+        
+        if(root == A || root == B){
+            return root;
+        }
+        if(left != null && right != null){
+            return root;
+        } else if(left != null) {
+            return left;
+        } else if(right != null) {
+            return right;
+        } else {
+            return null;
+        }
+        
+    }
+}
