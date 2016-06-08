c15f9a28d0b3d0dce8c9b7f0a4de63ccf0785668
xxxxxl <lix0212@hotmail.com>
2016-06-08 01:54:59 -0400

    Update LCA.java

--- Data-Structure/BinaryTree/LCA.java
+++ Data-Structure/BinaryTree/LCA.java
@@ -39,3 +39,46 @@ public class Solution {
         
     }
 }
+
+/** Second Solution if have parent pointer*/
+/**
+ * Definition of ParentTreeNode:
+ * 
+ * class ParentTreeNode {
+ *     public ParentTreeNode parent, left, right;
+ * }
+ */
+public class Solution {
+    /**
+     * @param root: The root of the tree
+     * @param A, B: Two node in the tree
+     * @return: The lowest common ancestor of A and B
+     */
+    public ParentTreeNode lowestCommonAncestorII(ParentTreeNode root,
+                                                 ParentTreeNode A,
+                                                 ParentTreeNode B) {
+        // Write your code here   
+        ArrayList<ParentTreeNode> pathA = getPath(A);
+        ArrayList<ParentTreeNode> pathB = getPath(B);
+        for (int i = 0; i < pathA.size() ; i++ ){
+            for (int j = 0; j < pathB.size(); j++){
+                if(pathA.get(i).val == pathB.get(j).val){
+                    return pathA.get(i);
+                }
+            }
+        }
+        return null;
+    }
+    // Get the path from root to current node.
+    public ArrayList<ParentTreeNode> getPath(ParentTreeNode root){
+        ArrayList<ParentTreeNode> path = new ArrayList<ParentTreeNode>();
+        if (root == null ){  
+            return path;
+        }
+        while(root != null ){
+            path.add(root);
+            root = root.parent;
+        }
+        return path;
+    }
+}
