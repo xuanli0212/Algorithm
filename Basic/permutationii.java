07b508d580b53aa9fbeaafbdc67855057a81c221
xxxxxl <lix0212@hotmail.com>
2016-06-18 21:01:24 -0400

    Create permutationii.java

---
+++ Basic/permutationii.java
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param nums: A list of integers.
+     * @return: A list of unique permutations.
+     */
+    public ArrayList<ArrayList<Integer>> permuteUnique(ArrayList<Integer> nums) {
+        // write your code here
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        if (nums ==  null || nums.isEmpty()) {
+            return result;
+        }
+        Collections.sort(nums);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int[] visited = new int[nums.size()];
+        helper(result, list, visited, nums);
+        return result;
+    }
+    private void helper(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> list, int[] visited, ArrayList<Integer> nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (visited[i] == 1 || (i != 0 && nums.get(i - 1) == nums.get(i) && visited[i - 1] == 0)) {
+                continue;
+            }
+            list.add(nums.get(i));
+            visited[i] = 1;
+            helper(result, list, visited, nums);
+            list.remove(list.size() - 1);
+            visited[i] = 0;
+        }
+        if (list.size() == nums.size()){
+            result.add(new ArrayList<Integer>(list));
+        }
+    }
+}
