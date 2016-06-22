839b0af7078b0f77f6a6e058720151e3e7627610
xxxxxl <lix0212@hotmail.com>
2016-06-22 00:15:43 -0400

    Update jumpgame.java

--- DP/jumpgame.java
+++ DP/jumpgame.java
@@ -1,3 +1,6 @@
+/**
+ *  Dynamic Programming Method.  The time comlexity is O(N^2)
+ */
 public class Solution {
     /**
      * @param A: A list of integers
@@ -23,3 +26,29 @@ public class Solution {
         return f[n - 1];
     }
 }
+/*
+* Greedy Method, The time complexity is O(N), and it also save space. Better way to solve this problem
+*/
+
+public class Solution {
+    /**
+     * @param A: A list of integers
+     * @return: The boolean answer
+     */
+    public boolean canJump(int[] A) {
+        // wirte your code here
+        if (A ==  null || A.length == 0) {
+            return true;
+        }
+        int n = A.length;
+        int farthest = A[0];
+        for (int i = 1; i < n; i++) {
+            //能到达i并且i可以走的更远。
+            if (i <= farthest && i + A[i] > farthest) {
+                farthest = i + A[i];
+            }
+        }
+        return farthest >= n - 1;
+    }
+}
+
