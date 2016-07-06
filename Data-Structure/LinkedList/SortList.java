740d587874b55303092522fa793b0cf170762ed5
xxxxxl <lix0212@hotmail.com>
2016-07-06 00:18:48 -0400

    Update SortList.java

--- Data-Structure/LinkedList/SortList.java
+++ Data-Structure/LinkedList/SortList.java
@@ -65,3 +65,88 @@ public class Solution {
 /* QuickSort version
 * 
 */
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The head of linked list.
+     * @return: You should return the head of the sorted linked list,
+                    using constant space complexity.
+     */
+    public ListNode sortList(ListNode head) {  
+        // write your code here
+        if (head == null || head.next == null) {
+            return head;
+        }
+        //partiton
+        ListNode mid = mid(head);
+        ListNode midHead = new ListNode(0);
+        ListNode midTail = midHead;
+        ListNode leftHead = new ListNode(0);
+        ListNode leftTail = leftHead;
+        ListNode rightHead = new ListNode(0);
+        ListNode rightTail = rightHead;
+
+        while (head != null) {
+            if (head.val < mid.val) {
+                leftTail.next = head;
+                leftTail = leftTail.next;
+            } else if (head.val > mid.val){
+                rightTail.next = head;
+                rightTail = rightTail.next;
+            } else {
+                midTail.next = head;
+                midTail = midTail.next;
+            }
+            head = head.next;
+        }
+        
+        // sort
+        leftTail.next = null;
+        rightTail.next = null;
+        midTail.next = null;
+        ListNode left = sortList(leftHead.next);
+        ListNode right = sortList(rightHead.next);
+        // merge
+       
+        ListNode newHead = new ListNode(0);
+        ListNode point = newHead;
+        newHead.next = left;
+        newHead = tail(newHead);
+        newHead.next = midHead.next;
+        newHead = tail(newHead);
+        newHead.next = right;
+        return point.next;
+        
+    }
+    private ListNode mid(ListNode head) {
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    private ListNode tail(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        return tail;
+    }
+}
+
