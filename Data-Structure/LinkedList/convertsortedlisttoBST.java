647f178cd2133544d8a43ee7e0a81b38dd993632
xxxxxl <lix0212@hotmail.com>
2016-07-06 16:37:36 -0400

    Update convertsortedlisttoBST.java

--- Data-Structure/LinkedList/convertsortedlisttoBST.java
+++ Data-Structure/LinkedList/convertsortedlisttoBST.java
@@ -59,3 +59,62 @@ public class Solution {
 /* Version 2: O(N) time complexity 
 *
 */
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: a tree node
+     */
+    public ListNode current = null;
+    public TreeNode sortedListToBST(ListNode head) {  
+        // write your code here
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return new TreeNode (head.val);
+        }
+        ListNode cur = head;
+        int length = 0;
+        while (cur != null) {
+            cur = cur.next;
+            length++;
+        }
+        current = head;
+        return helper (0, length -1);
+        
+    }
+    private TreeNode helper (int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = start + (end - start)/2; 
+        TreeNode left = helper(start, mid - 1);
+        TreeNode root = new TreeNode(current.val);
+        current = current.next;
+        TreeNode right = helper(mid + 1, end);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+}
