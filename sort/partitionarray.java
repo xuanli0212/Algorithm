691262e9f9b1e23f9a0c1ace913ab6fd7fa47295
xxxxxl <lix0212@hotmail.com>
2016-07-16 04:41:47 -0400

    Create partitionarray.java

---
+++ sort/partitionarray.java
@@ -0,0 +1,29 @@
+public class Solution {
+	/** 
+     *@param nums: The integer array you should partition
+     *@param k: As description
+     *return: The index after partition
+     */
+    public int partitionArray(int[] nums, int k) {
+	    //write your code here
+	    
+	    int i = 0;
+	    int j = nums.length - 1;
+	    while (i <= j){
+	        while (i <= j && nums[i] < k) {
+	            i++;
+	        }
+	        while (i <= j && nums[j] >= k) {
+	            j--;
+	        }
+	        if (i < j) {
+	            int tmp = nums[i];
+	            nums[i] = nums[j];
+	            nums[j] = tmp;
+	            i++;
+	            j--;
+	        }
+	    }
+	    return j + 1;
+    }
+}
