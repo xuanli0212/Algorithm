3c6e8dcd14878889daa06ed427ddabb8ad3b719a
xxxxxl <lix0212@hotmail.com>
2016-09-25 02:16:23 -0700

    Update addbinary.java

--- bit/addbinary.java
+++ bit/addbinary.java
@@ -37,3 +37,39 @@ public class Solution {
         return result;
     }
 }
+//solution2
+public class Solution {
+    /**
+     * @param a a number
+     * @param b a number
+     * @return the result
+     */
+    public String addBinary(String a, String b) {
+        // Write your code here
+        if (a == null || a.length() == 0) return b;
+        if (b == null || b.length() == 0) return a;
+        int lengthA = a.length() - 1;
+        int lengthB = b.length() - 1;
+        //int length = lengthA > lengthB ? lengthA - 1 : lengthB - 1;
+        int sum = 0;
+        int carry = 0;
+        StringBuilder result = new StringBuilder();
+        while (lengthA >= 0 || lengthB >= 0) {
+            sum = carry;
+            if (lengthA >= 0) {
+                sum += a.charAt(lengthA) - '0';
+                lengthA--;
+            }
+            if (lengthB >= 0)  {
+                sum += b.charAt(lengthB) - '0';
+                lengthB--;
+            }
+            carry = sum / 2;
+            sum = sum % 2;
+            result.insert(0, sum);
+        }
+        if (carry != 0) result.insert(0, carry);
+        return result.toString();
+        
+    }
+}
